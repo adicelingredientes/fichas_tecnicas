869513ae2508374957647d895f2ebf9b9fac41e3
Adicel Ingredientes <autimacao@adicel.com.br>
2025-01-31 09:46:17 -0300

    rev.

--- uploader ficha tecnica.ps1
+++ uploader ficha tecnica.ps1
@@ -5,5 +5,6 @@ while ($true) {
 	git remote add origin https://github.com/adicelingredientes/fichas_tecnicas.git
 	git checkout -b main
 	git push origin main --force
+	echo atualizado
 	Start-Sleep -Seconds 86400
 }
